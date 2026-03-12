ac95df0868ed80600118d5f56d9e75a87faedde3
Tonkis-code <toni.wecksell@gmail.com>
2026-03-12 14:12:29 +0100

    created a post and some testdata

--- NETAPI/Program.cs
+++ NETAPI/Program.cs
@@ -3,6 +3,7 @@ var app = builder.Build();
 
 app.MapGet("/", () => "API is running.");
 
+// GET Request for the pokemons inside of the pokemons variable
 app.MapGet("/pokemons", () =>
 {
     var pokemons = new[]
@@ -15,4 +16,35 @@ app.MapGet("/pokemons", () =>
     return pokemons;
 });
 
+var attacks = [
+    Id = attack1,
+    request.Name = "Tackle",
+    request.Damage = 40,
+    request.Type = "Normal",
+
+
+];
+
 app.Run();
+
+// POST Request for an attack
+app.MapPost("/attack", (AttackRequest request) =>
+{
+    var attack = new
+    {
+        Id = 1,
+        Name = request.Name,
+        Damage = request.Damage,
+        Type = request.Type
+    };
+
+    return attack;
+});
+
+public class AttackRequest
+{
+    public string Name { get; set; }
+    public int Damage { get; set; }     // int because its a number not a string (i forgot)
+    public string Type { get; set; }
+}
+
