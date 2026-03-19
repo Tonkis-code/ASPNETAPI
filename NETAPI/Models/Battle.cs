0d6e1d734aaa11084c9bccdc1faf06b938589eda
Tonkis-code <toni.wecksell@gmail.com>
2026-03-19 15:04:59 +0100

    fixed strings to ints

--- NETAPI/Models/Battle.cs
+++ NETAPI/Models/Battle.cs
@@ -3,7 +3,7 @@ public class Battle
     public int Id { get; set; }
     public int PlayerPokemonId { get; set; }
     public int EnemyPokemonId { get; set; }
-    public int PlayerHp { get; set; } = "100";
-    public int EnemyHp { get; set; } = "100";
-    public bool IsOver { get; set; } = "false";
+    public int PlayerHp { get; set; } = 100;
+    public int EnemyHp { get; set; } = 100;
+    public bool IsOver { get; set; } = false;
 }
